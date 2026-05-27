84d8b89b98242056bf9f303247e2d5d591ada1ef
Corey Wilson <72322079+CoreyWilson319@users.noreply.github.com>
2026-05-27 16:54:54 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ function App() {
   return (
     <div>
       <h1 className='title'>Smite 2</h1>
-      <p>Smite is a M.O.B.A. style game that uses mythological and religious deitiies and heroes for characters in which the objective for the game mode that I play is to kill the enemy titan. I make no claims about being good it's just something that I've been able to enjoy recently putting time into it when I'm not working on something that is work or school related. Below are a few of my favorite characters to play as especially as a front liner role.</p>
+      <p>Smite is a M.O.B.A. style game that uses mythological and religious deitiies and heroes for characters 
+        in which the objective for the game mode that I play is to kill the enemy titan. I make no claims about 
+        being good it's just something that I've been able to enjoy recently putting time into it when I'm not 
+        working on something that is work or school related. Below are a few of my favorite characters to play 
+        as especially as a front liner role. When trying to explain my take on MOBAs to those who don't know what
+        they are I relate them to sports in how every position has a role to play to help the team succeed.
+        </p>
       <Main></Main>
     </div>
 
