dd6ad2f5065074a328829d1f9c9ffdf3f59dbed4
Corey Wilson <72322079+CoreyWilson319@users.noreply.github.com>
2026-05-27 16:41:02 -0400

    Initial Commit
    test

---
+++ src/Icons.js
@@ -0,0 +1,20 @@
+import awilix from "./god portraits/t_GodPortrait_Awilix.avif"
+import gilgamesh from "./god portraits/t_GodPortrait_Gilgamesh.avif"
+import ganesha from "./god portraits/t_GodPortrait_Ganesha.avif"
+
+
+function Icons() {
+    return(
+        <div className="icons">
+            <img id="icon" alt="Awilix" src={awilix}></img>
+            <p>Awilix is a newer release, she was one of my favorite Gods to play in Smite 1. She's good at chasing other characters down and has a mount of her own named Suku.</p>
+            <img id="icon" alt="Gilgamesh" src={gilgamesh}></img>
+            <p>Gilgamesh is another new release to Smite 2 returning from Smite 1. His kit involves slowing enemies and rewarding himself and teammates for landing abilities from his kit. Lore wise he's probably one of my favorites.</p>
+            <img id="icon" alt="Ganesha" src={ganesha}></img>
+            <p>Ganesha has been in the game for some time now and is all about helping his teammates succede. He cannot receive credit for killing an enemy if a teammate is around the teammate will receive that credit.</p>
+        </div>
+
+    )
+}
+
+export default Icons
---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import Icons from "./Icons"
+
+function Main() {
+  return (
+    <div className="main">
+        <Icons></Icons>
+    </div>
+
+  );
+}
+
+export default Main;
